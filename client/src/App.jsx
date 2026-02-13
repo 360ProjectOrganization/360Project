11aa8270297db45adc13e3ea0a1bda03c54a3246
Timmi Draper <timmid999@gmail.com>
2026-02-13 13:59:59 -0800

    adding pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import RegisterApplicantPage from './pages/RegisterApplicantPage.jsx';
+import RegisterEmployerPage from './pages/RegisterEmployerPage.jsx';
 
 function App() {
-  return (
-    <div className="app">
-      <Routes>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Routes>
+                <Route path="/register-applicant" element={<RegisterApplicantPage />} />
+                <Route path="/register-employer" element={<RegisterEmployerPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
