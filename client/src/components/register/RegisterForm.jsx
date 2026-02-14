d065a08ad32c0ad8af5ea51222f08449153f0983
Timmi Draper <timmid999@gmail.com>
2026-02-14 00:54:52 -0800

    actual register info

--- client/src/components/register/RegisterForm.jsx
+++ client/src/components/register/RegisterForm.jsx
@@ -1,31 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function RegisterForm({ role }) {
     const isEmployer = role === "employer";
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log({name, email, password, confirmPassword}) /*no security beacyuse idc rn */
+    }
+
     return (
         <section className="registerCard">
             <h2>You are registering as an {isEmployer ? "employer" : "applicant"}</h2>
 
-            <form> {/*only inputs and button */}
+            <form onSubmit={handleSubmit}> {/*only inputs and button */}
                 <section className="nameInputSection">
                     <label>{isEmployer ? "Company Name" : "Full Name"}</label>
-                    <input type="text"/>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
                 </section>
 
                 <section className="emailInputSection">
                     <label>Email</label>
-                    <input type="text"/>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </section>
 
                 <section className="passwordInputSection">
                     <label>Password</label>
-                    <input type="password"/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </section>
 
                 <section className="confirmPasswordInputSection">
                     <label>Confirm Password</label>
-                    <input type="password"/>
+                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </section>
 
                 <button type="submit">Register</button>
