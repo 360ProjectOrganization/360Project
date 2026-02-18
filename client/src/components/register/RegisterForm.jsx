8764fb909059c0ad3bd59017005b68de89ae5e60
Timmi Draper <timmid999@gmail.com>
2026-02-17 20:48:09 -0800

    validation

--- client/src/components/register/RegisterForm.jsx
+++ client/src/components/register/RegisterForm.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { validateRegisterForm } from "../../utils/validation/validateRegisterForm";
+
 export default function RegisterForm({ role }) {
     const isEmployer = role === "employer";
 
+    const [errors, setErrors] = useState({});
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -11,7 +14,14 @@ export default function RegisterForm({ role }) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log({name, email, password, confirmPassword}) /*no security beacyuse idc rn */
+        
+        const inputErrors = validateRegisterForm({ name, email, password, confirmPassword }, role);
+        setErrors(inputErrors);
+
+        //if errors is oempty then input is valid
+        if (Object.keys(inputErrors).length > 0) {
+            return;
+        }
     }
 
     return (
@@ -21,22 +31,26 @@ export default function RegisterForm({ role }) {
             <form onSubmit={handleSubmit}> {/*only inputs and button */}
                 <section className="nameInputSection">
                     <label>{isEmployer ? "Company Name" : "Full Name"}</label>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    {errors.name && <p className="error">{errors.name}</p>}
                 </section>
 
                 <section className="emailInputSection">
                     <label>Email</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    {errors.email && <p className="error">{errors.email}</p>}
                 </section>
 
                 <section className="passwordInputSection">
                     <label>Password</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {errors.password && <p className="error">{errors.password}</p>}
                 </section>
 
                 <section className="confirmPasswordInputSection">
                     <label>Confirm Password</label>
-                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
                 </section>
 
                 <button type="submit">Register</button>
