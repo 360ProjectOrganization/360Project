338a40044b6bfaaf5a45a370b67483713b770b52
Carson Bennett <carben2004@gmail.com>
2026-02-18 17:05:45 -0800

    Added header to main, added styling to header, starting dropdown

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import "./Header.css"
+
+function Header() {
+
+    return (
+        <>
+            <section id="header-container">
+                <section id="jobly-container">
+                    <a href="/">JobLy</a>
+                </section>
+
+                <section id="navigation-container">
+                    <button className="header-button-company">
+                        <a href="/"> {/* To be added later */}
+                            Company Portal
+                        </a>
+                    </button>
+
+                    <button className="header-button-admin">
+                        <a href="/"> {/* To be added later */}
+                            Admin Portal
+                        </a>
+                    </button>
+                </section>
+
+                <section id="user-profile">
+                    <button className="header-profile">
+                        <a>Profile</a>
+                    </button>
+                </section>
+            </section>
+        </>
+    );
+};
+
+export default Header;
