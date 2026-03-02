a8a8c9a070d64bd1f73952bdcc5d4122ea57e5bb
GH-USR-01 <mail.peter02@proton.me>
2026-03-02 13:08:17 -0800

    update models to handle pfps

--- server/repository/models/company.model.js
+++ server/repository/models/company.model.js
@@ -6,7 +6,8 @@ const companySchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, trim: true, lowercase: true },
   password: { type: String, required: true },
-  pfp: { type: Buffer }, // binary image jpeg or a png
+  pfp: { type: Buffer },
+  pfpContentType: { type: String, default: 'image/jpeg' },
   jobPostings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'JobPosting' }],
 }, { timestamps: true });
 
