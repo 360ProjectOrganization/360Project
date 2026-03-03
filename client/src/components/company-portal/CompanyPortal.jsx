c8139ba4dee9d67ad5bea660c95241ca390b9e7e
Timmi Draper <timmid999@gmail.com>
2026-03-02 20:47:06 -0800

    adding onclicks, just postings rn

--- client/src/components/company-portal/CompanyPortal.jsx
+++ client/src/components/company-portal/CompanyPortal.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CompanyPortalSubNav from "./CompanyPortalSubNav.jsx";
+import CompanyPortalJobPostings from "./CompanyPortalJobPostings.jsx";
 
 export default function CompanyPortal() {
+    const [activeView, setActiveview] = useState("postings"); //default view is postings
+
+    const handlePostingsClick = () => {
+        // display all job postings for a company
+        setActiveview("postings");
+    };
+
+    const handleCreateClick = () => {
+        // opens modal for creating a job posting
+    };
+
+    const handleProfileClick = () => {
+        //TODO: once company profile is created, add navigate here
+    };
+
     return (
         <>
-            <CompanyPortalSubNav/>
+            <CompanyPortalSubNav
+                onPostingsClick={handlePostingsClick}
+                onCreateClick={handleCreateClick}
+                onProfileClick={handleProfileClick}
+            />
+            
+            {activeView === "postings" && <CompanyPortalJobPostings/>}
         </>
     );
 }
--- client/src/components/company-portal/CompanyPortalSubNav.jsx
+++ client/src/components/company-portal/CompanyPortalSubNav.jsx
@@ -1,11 +1,11 @@
 import "./CompanyPortalSubNav.css";
 
-export default function CompanyPortalSubNav() {
+export default function CompanyPortalSubNav({onPostingsClick, onCreateClick, onProfileClick}) {
     return (
         <section className="subnav-container">
-            <button className="company-portal-btn">Company Job Postings</button>
-            <button className="company-portal-btn">Create New Job Posting</button>
-            <button className="company-portal-btn">Company Profile</button>
+            <button className="company-portal-btn" onClick={onPostingsClick}>Company Job Postings</button>
+            <button className="company-portal-btn" onClick={onCreateClick}>Create New Job Posting</button>
+            <button className="company-portal-btn" onClick={onProfileClick}>Company Profile</button>
         </section>
     );
 }
