2c7aded642ff7530f04fda4813d214173c1b8338
Timmi Draper <timmid999@gmail.com>
2026-03-03 03:22:35 -0800

    error checking

--- client/src/components/company-portal/CreateJobForm.jsx
+++ client/src/components/company-portal/CreateJobForm.jsx
@@ -40,17 +40,42 @@ export default function CreateJobForm({ companyId, onSuccess }) {
             <div className="job-posting-form-body">
                 <div className="form-row">
                     <label>Title</label>
-                    <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
+                    <div className="input-wrapper">
+                        <input id="title" type="text" value={title} onChange={(e) => {
+                            setTitle(e.target.value)
+                            if (errors.title) {
+                                setErrors(prev => ({ ...prev, title: undefined }));
+                            }
+                        }} placeholder="Title" />
+                        {errors.title && <span className="error-text">{errors.title}</span>}
+                    </div>
                 </div>
 
                 <div className="form-row">
                     <label>Location</label>
-                    <input id="location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Location" />
+                    <div className="input-wrapper">
+                        <input id="location" type="text" value={location} onChange={(e) => {
+                            setLocation(e.target.value)
+                            if (errors.location) {
+                                setErrors(prev => ({ ...prev, location: undefined }));
+                            }
+                        }} placeholder="Location" />
+                        {errors.location && <span className="error-text">{errors.location}</span>}
+                
+                    </div>
                 </div>
-
+                
                 <div className="form-row form-row-description">
                     <label>Description</label>
-                    <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
+                    <div className="input-wrapper">
+                        <textarea id="description" value={description} onChange={(e) => {
+                            setDescription(e.target.value)
+                            if (errors.description) {
+                                setErrors(prev => ({ ...prev, description: undefined }));
+                            }
+                        }} placeholder="Description" />
+                        {errors.description && <span className="error-text">{errors.description}</span>}
+                    </div>
                 </div>
             </div>
 
