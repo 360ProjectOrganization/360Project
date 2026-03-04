09eeb39c0500d65e8ec9c4da40a2070a68c78067
Timmi Draper <timmid999@gmail.com>
2026-03-04 15:51:51 -0800

    adding todo for later

--- client/src/components/home/HomeSubNav.jsx
+++ client/src/components/home/HomeSubNav.jsx
@@ -3,6 +3,7 @@ import "../company-portal/CompanyPortal.css";
 export default function HomeSubNav() {
     return (
         <section className="subnav-container">
+            {/* TODO: switch location and job title to search inputs, change Tags to a drop down */}
             <button className="company-portal-btn">Search: Location...</button>
             <button className="company-portal-btn">Search: Job Title...</button>
             <button className="company-portal-btn">Drop-down: Tags</button>
