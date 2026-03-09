ffeda3949d6a0f2d5cf4335b548fe9938255a640
Timmi Draper <timmid999@gmail.com>
2026-03-09 13:41:26 -0700

    header css

--- client/src/components/home/HomeSearchBar.jsx
+++ client/src/components/home/HomeSearchBar.jsx
@@ -1,18 +1,23 @@
-
+import "../company-portal/CompanyPortal.css";
+import "../home/Home.css";
 
 export default function HomeSearchBar() {
     return (
-        <header className="home-header">
-            <label>Job Title</label>
-            <input type="search" className="home-search" placeholder="Job Title" />
+        <header className="subnav-container">
+            <section className="home-search">
+                <label>Job Title</label>
+                <input type="search" className="home-search" placeholder="Job Title" />
+            </section>
 
-            <label>Location</label>
-            <input type="search" className="home-search" placeholder="Location" />
+            <section className="home-search">
+                <label>Location</label>
+                <input type="search" className="home-search" placeholder="Location" />
+            </section>
 
-            <select name="tags" id="tags">
-                <option value="tag1">Tag1</option>
-                <option value="tag2">Tag2</option>
-                <option value="tag3">Tag3</option>
+            <select className="home-tags" name="tags" id="tags">
+                <option value="tag1">bro1</option>
+                <option value="tag2">bro2</option>
+                <option value="tag3">bro3</option>
             </select>
         </header>
     );
