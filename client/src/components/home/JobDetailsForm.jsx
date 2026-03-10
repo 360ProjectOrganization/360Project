6ba3a1ddb45e6e4a7a8dba972bb106123001b91e
Timmi Draper <timmid999@gmail.com>
2026-03-09 19:31:50 -0700

    WAY BETTER than switch

--- client/src/components/home/JobDetailsForm.jsx
+++ client/src/components/home/JobDetailsForm.jsx
@@ -1,33 +1,25 @@
 import { jobPostingApi } from "../../utils/api.js";
 import JobPostingForm from "../company-portal/JobPostingForm.jsx";
 
+import CompanyJobPostingDetails from "./JobPostingModals/CompanyJobPostingDetails.jsx";
+import ApplicantJobPostingDetails from "./JobPostingModals/ApplicantJobPostingDetails.jsx";
+import AdminJobPostingDetails from "./JobPostingModals/AdminJobPostingDetails.jsx";
+
 export default function JobDetailsForm({ posting, role, onSuccess, onCancel }) {
 
-    switch (role) {
-        case "company":
-            return (
-                <>
-                    COMPANY
-                </>
-            );
-        case "applicant":
-            return (
-                <>
-                    APPLICANT
-                </>
-            );
-        case "administrator":
-            return (
-                <>
-                    ADMIN
-                </>
-            );
-        default:
-            return (
-                <>
-                    need to login / register
-                </>
-            );
+    const roleComponents = {
+        company: CompanyJobPostingDetails,
+        applicant: ApplicantJobPostingDetails,
+        administrator: AdminJobPostingDetails
+    };
+
+    const RoleComponent = roleComponent[role];
+
+    if (!RoleComponent) {
+        return <>need to login / register</> // TODO: reroute them after saving the post they're wanting to look at somehow (figure out later)
     }
 
+    return (
+        <RoleComponent posting={posting} onSuccess={onSuccess} onCancel={onCancel} />
+    );
 }
