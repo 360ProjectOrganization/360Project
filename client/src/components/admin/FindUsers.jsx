b7274993e3dc48cab4ea79c6eaef1e42fd15b6d5
James Birnie <jkbirnie@gmail.com>
2026-03-12 10:32:00 -0700

    "got rid of test data"

--- client/src/components/admin/FindUsers.jsx
+++ client/src/components/admin/FindUsers.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react"
+import { applicantApi } from "../../utils/api";
+
 export default function FindUsers({filterType, filter}){
+    const [filteredCards, setFilteredCards] = useState();
+    useEffect(() => {
+        const response = applicantApi.getAll();
+        
+    }, [filteredCards]);
+
     return(
         <section>
             
