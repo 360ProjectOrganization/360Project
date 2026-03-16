23e1ad732798bcd34facfcf3293016c6ca59c29c
Carson Bennett <carben2004@gmail.com>
2026-03-15 21:43:04 -0700

    working on edit profile section

--- client/src/components/profile-page/EditProfileForm.jsx
+++ client/src/components/profile-page/EditProfileForm.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./EditProfileForm.css"
 
 function EditProfileForm(){
+
     return(
         <>
             <div id="edit-profile-section">
@@ -9,13 +10,22 @@ function EditProfileForm(){
                     <h2>Edit Profile</h2>
                     <div className="update-section">
                         <p>Change Email?</p>
+                        
+                        <label>New Email: </label>
+                        <input type="text"/>
+
+                        <br />
+                        <label>Password: </label>
+                        <input type="password"/>
+
+                        <br />
                         <button id="change-email-button">
                             Update Email
                         </button>
                     </div>
 
                     <div className="update-section">
-                        <p>Change Password?</p>
+                        <p>Change Password</p>
                         <button id="change-password-button">
                             Update Password
                         </button>
