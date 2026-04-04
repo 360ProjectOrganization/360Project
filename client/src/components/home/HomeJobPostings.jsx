72ab0c5ab146f33a2828187fe91fe694cbf49576
Timmi Draper <timmid999@gmail.com>
2026-04-03 21:16:22 -0700

    removing unnecessary code

--- client/src/components/home/HomeJobPostings.jsx
+++ client/src/components/home/HomeJobPostings.jsx
@@ -100,7 +100,6 @@ export default function HomeJobPostings() {
                 );
 
                 const postings = postingsByCompany.flat();
-                postings.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
 
                 setJobPostings(postings);
             }
