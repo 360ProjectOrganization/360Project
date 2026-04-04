1ef4802b536a33573f1a3d71ca47714bf03d1510
Timmi Draper <timmid999@gmail.com>
2026-04-03 19:50:48 -0700

    tests folder

--- client/src/tests/App.test.jsx
+++ client/src/tests/App.test.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react' //render shows components in fake DOM and screen lets us query what's on the page
 import { MemoryRouter } from 'react-router-dom' // fake router
-import App from './App'
+import App from '../App'
 
 // Mock each page component (basically replaces the real pages with fake ones so we can test routing)
-jest.mock('./pages/HomePage', () => () => <div>Home Page</div>)
-jest.mock('./pages/RegisterPage', () => () => <div>Register Page</div>)
-jest.mock('./pages/LoginPage', () => () => <div>Login Page</div>)
-jest.mock('./pages/ProfilePage', () => () => <div>Profile Page</div>)
-jest.mock('./pages/CompanyPortalPage', () => () => <div>Company Portal Page</div>)
-jest.mock('./pages/AdminPage', () => () => <div>Admin Page</div>)
+jest.mock('../pages/HomePage', () => () => <div>Home Page</div>)
+jest.mock('../pages/RegisterPage', () => () => <div>Register Page</div>)
+jest.mock('../pages/LoginPage', () => () => <div>Login Page</div>)
+jest.mock('../pages/ProfilePage', () => () => <div>Profile Page</div>)
+jest.mock('../pages/CompanyPortalPage', () => () => <div>Company Portal Page</div>)
+jest.mock('../pages/AdminPage', () => () => <div>Admin Page</div>)
 
 // sets the url to whatever is passed in and renders the app, reusable for tests
 function renderWithRoute(route) {
