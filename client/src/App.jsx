bac50714b11a75fdbeea5b27f7a2bfad233e9d09
Timmi Draper <timmid999@gmail.com>
2026-04-04 14:59:09 -0700

    adding bad navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import NotFound from "./pages/NotFound.jsx";
 import HomePage from './pages/HomePage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
@@ -16,6 +17,8 @@ function App() {
                 <Route path='/profile' element={<ProfilePage />} />
                 <Route path='/company-portal' element={<CompanyPortalPage />} />
                 <Route path='/Admin' element={<AdminPage />} />
+
+                <Route path="*" element={<NotFound />} /> {/* !!! MUST BE LAST or it will catch everything */}
             </Routes>
         </div>
     );
