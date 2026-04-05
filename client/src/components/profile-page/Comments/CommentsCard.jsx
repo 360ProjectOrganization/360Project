f5b6322c747d633ef58c7f38a67c22281523e17f
Timmi Draper <timmid999@gmail.com>
2026-04-05 14:38:13 -0700

    adding buttons

--- client/src/components/profile-page/Comments/CommentsCard.jsx
+++ client/src/components/profile-page/Comments/CommentsCard.jsx
@@ -5,10 +5,15 @@ export default function CommentsCard({ jobTitle, date, content }) {
     return (
         <div className="user-comment-card">
             <section className="comment-header">
-                <div className="comment-title">{jobTitle && <strong>{jobTitle}</strong>}</div>
+                <section className="comment-header-left">
+                    <div className="comment-title">{jobTitle && <strong>{jobTitle}</strong>}</div>
+                    <button className="comment-btn">Edit</button>
+                    <button className="comment-btn">Delete</button>
+                    <button className="comment-btn">View Job Post</button>
+                </section>
                 <div className="comment-date">{formatDate(date)}</div>
             </section>
-            
+
             <hr />
 
             <div className="comment-content">
