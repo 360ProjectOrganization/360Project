b39a6ebd398d3b0eda8e7c5322d08181581a3271
Carson Bennett <carben2004@gmail.com>
2026-04-05 20:59:43 -0700

    fixed issue with pfp generation, resume viewing now works

--- client/src/components/common/Row.jsx
+++ client/src/components/common/Row.jsx
@@ -1,31 +1,40 @@
 import "./Row.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { applicantApi } from "../../utils/api";
+import Modal from "./Modal";
+import ViewResumeForm from "../company-portal/ViewResumeForm";
 
 export default function Row({ name, id }){
     const [image, setImage] = useState("");
-    async function getApplicantPfp(){
-        if(id){
-            const url = applicantApi.getPfpUrl(id);
-            let response = await fetch(url, {
-                method: "GET"
-            });
-        
-            if(response.status === 200){
-                const imageBlob = await response.blob();
-                const imageObjectURL = URL.createObjectURL(imageBlob);
-                console.log(imageObjectURL);
-                setImage(imageObjectURL);
+    const [resumeOptions, setResumeOptions] = useState(false);
+    useEffect(() => {
+        async function getApplicantPfp(){
+            if(id){
+                const url = applicantApi.getPfpUrl(id);
+                let response = await fetch(url, {
+                    method: "GET"
+                });
+            
+                if(response.status === 200){
+                    const imageBlob = await response.blob();
+                    const imageObjectURL = URL.createObjectURL(imageBlob);
+                    setImage(imageObjectURL);
+                }
             }
         }
-    }
-    getApplicantPfp();
+        getApplicantPfp();
+    }, [])
 
     return(
-        <section className="row-container">
-            <img src={image} alt="pfp"/>
-            <h3>{name}</h3>
-            <button id="row-resume-btn">View Resume</button>
-        </section>
+        <>
+            <section className="row-container">
+                <img src={image} alt="pfp"/>
+                <h3>{name}</h3>
+                <button id="row-resume-btn" onClick={() => setResumeOptions(true)}>View Resume</button>
+            </section>
+            <Modal isOpen={resumeOptions} onClose={() => setResumeOptions(false)} title={"Viewing Options"} size={"small"}>
+                <ViewResumeForm applicantId={id} />
+            </Modal>
+        </>
     )
 }
