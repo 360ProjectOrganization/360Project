d5fd3494074dd1b48aa44a50e7d4315266b36627
Timmi Draper <timmid999@gmail.com>
2026-04-07 13:39:11 -0700

    Breadcrumbs tests

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': '<rootDir>/jest.styleMock.cjs',
+    },
     transform: {
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
