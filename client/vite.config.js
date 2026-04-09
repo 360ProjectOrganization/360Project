7a6bd724608f52e68368a928010b71b9aca88005
Carson Bennett <carben2004@gmail.com>
2026-04-09 00:44:53 -0700

    docker files added

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     hmr: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://server-dev:5000',
         changeOrigin: true,
       },
     },
