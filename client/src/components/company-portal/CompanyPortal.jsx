f398550abc1fc17b08fccbe0a3faa8dca5084e67
Timmi Draper <timmid999@gmail.com>
2026-04-10 12:56:12 -0700

    cleaning up header on company portal, fixing broken test

--- client/src/components/company-portal/CompanyPortal.jsx
+++ client/src/components/company-portal/CompanyPortal.jsx
@@ -1,18 +1,16 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import CompanyPortalSubNav from "./CompanyPortalSubNav.jsx";
 import CompanyPortalJobPostings from "./CompanyPortalJobPostings.jsx";
 import CreateJobForm from "./CreateJobForm.jsx";
 import Modal from "../common/Modal.jsx";
 
 export default function CompanyPortal() {
-    const navigate = useNavigate();
     const location = useLocation();
-    const [activeView, setActiveview] = useState("postings"); //default view is postings
     const [selectedPosting, setSelectedPosting] = useState(null);
     const [openModal, setOpenModal] = useState(null);
     const [refreshKey, setRefreshKey] = useState(0);
-    
+
     const editPostingId = location.state?.editPostingId;
 
     //get company ID from local storage
@@ -21,42 +19,27 @@ export default function CompanyPortal() {
     const companyId = user?._id;
     const companyName = user?.name;
 
-    const handlePostingsClick = () => {
-        setActiveview("postings");
-    };
-
     const handleCreateJobPostingClick = () => {
         setOpenModal("open-modal");
     };
 
-    const handleProfileClick = () => {
-        navigate("/profile");
-    };
-
-    // Which subnav button should look selected (create modal counts as "create" tab)
-    const activeSubnavTab = openModal === "open-modal" ? "create" : "postings";
-
     return (
         <>
             <CompanyPortalSubNav
-                activeTab={activeSubnavTab}
-                onPostingsClick={handlePostingsClick}
+                isCreateActive={openModal === "open-modal"}
                 onCreateClick={handleCreateJobPostingClick}
-                onProfileClick={handleProfileClick}
             />
-            
-            {activeView === "postings" && (
-                <CompanyPortalJobPostings
-                    companyId={companyId}
-                    companyName={companyName}
-                    refreshKey={refreshKey}
-                    editPostingId={editPostingId}
-                    onEditPosting={(posting) => {
-                        setSelectedPosting(posting);
-                        setOpenModal("edit");
-                    }}
-                />
-            )}
+
+            <CompanyPortalJobPostings
+                companyId={companyId}
+                companyName={companyName}
+                refreshKey={refreshKey}
+                editPostingId={editPostingId}
+                onEditPosting={(posting) => {
+                    setSelectedPosting(posting);
+                    setOpenModal("edit");
+                }}
+            />
 
             <Modal isOpen={openModal === "open-modal"} onClose={() => setOpenModal(false)} title="Create New Job Posting">
                 <CreateJobForm companyId={companyId} onSuccess={() => { setOpenModal(false); setRefreshKey((k) => k + 1); }} onCancel={() => setOpenModal(false)}/>
